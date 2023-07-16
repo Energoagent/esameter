f8d515446525ebade87464231cfa6aa566227c05
energoagent <kovalevvb@hotmail.com>
2023-07-16 18:46:52 +0300

    1

--- meter_simulator.py
+++ meter_simulator.py
@@ -1,31 +1,33 @@
 import os
 import openpyxl
-
+    
 LOGDATA = {}
 
 def BDC_Code(integer_input):
-    integer_string=str(integer_input)
-    int_hex_input= int(integer_string, 16)  # Переводим из 16-ричной в десятичную
-    binary_output= bin(int_hex_input)[2:].zfill(len(integer_string.strip())*4)  # на каждую исходную цифру по 4 двоичных
+    try:
+        integer_string=str(integer_input)
+        int_hex_input= int(integer_string, 16)  # Переводим из 16-ричной в десятичную
+        binary_output= bin(int_hex_input)[2:].zfill(len(integer_string.strip())*4)  # на каждую исходную цифру по 4 двоичных
+    except ValueError: pass
+#    except BaseException as e:
+#        print('EXCEPTION:', e)
     return(binary_output)
 
 def loaddata():
-# load datasheet
     try:
         wb = openpyxl.load_workbook('./logdata.xlsx')
     except OSError as err:
         print('ERROR:', err)
-        return False
     else: 
         ws = wb['DATA']
         if ws == None:
             print('ERROR:', 'sheet none')
-            return False
         else:
-            databyte=bytearray(13)
-            datadict = {}
             for row in ws:
-                try:    
+                try:
+                    databyte=bytearray(13)
+                    portkey = str(row[0].value)
+                    meterkey = str(row[1].value)
                     databyte[0]= int(BDC_Code(row[1].value),2)      #MeterAdress
                     databyte[1]= int(BDC_Code(row[6].value),2)      #секунды до
                     databyte[2]= int(BDC_Code(row[5].value),2)      #минуты до
@@ -39,21 +41,30 @@ def loaddata():
                     databyte[10]= int(BDC_Code(row[3].value),2)      #число      после коррекции
                     databyte[11]= int(BDC_Code(row[2].value),2)      #месяц      после коррекции
                     databyte[12]= int(BDC_Code(23),2)              #год        после коррекции
-                    datadict[str(row[1].value)] = databyte
-                    LOGDATA[str(row[0].value)] = datadict 
-                except ValueError as err: pass
-#                    print('ERROR:', err)
-#            for key, value in LOGDATA.items():
-#                print(key, '  VALUE:', value)
-        return True
+                    if portkey in LOGDATA:
+                        if meterkey in LOGDATA[portkey]:
+                            LOGDATA[portkey][meterkey].append(databyte)
+                        else:
+                            LOGDATA[portkey] = {meterkey: [databyte]}
+                    else:
+                        LOGDATA[portkey] = {meterkey: [databyte]}
+                except ValueError: pass
+#                    print(databyte)
+#                    print(portkey, meterkey)
+#                    for cell in row:
+#                        print('-----', cell.value)
+    return
 
 def GetMeterTimeJornal(socketNo,MeterAdress,RowNo):
-    databyte=LOGDATA[str(socketNo)][str(MeterAdress)]
+    databyte = LOGDATA[str(socketNo)][str(MeterAdress)][RowNo]
     return databyte
 
 #loaddata()
+#print(LOGDATA)
 #for key, value in LOGDATA.items():
-    for key2, value2 in LOGDATA[key].items():
-        print(key, ' ', key2, '  VALUE:', value2)
-
-#print('DATA:', GetMeterTimeJornal(55009,73,0))
+#    for key2, value2 in LOGDATA[key].items():
+#        print('DATA:', key, key2)
+#        for a1 in value2:
+#            print('----------------', a1)
+#print(GetMeterTimeJornal(55011,33,5))
+#print(GetMeterTimeJornal(55005,35,5))
